1aba94733eeacb6fc7e49ee0c4888e6bc07247d4
pyounghwa.yoon <pyounghwa.yoon@hlcompany.com>
2025-02-23 22:13:19 +0900

    modi main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 import argparse
 
 
-def parse_args(known=False) -> argparse.Namespace:
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str, default = None, help='directory of asm file to be translated')
-    args = parser.parse_known_args()[0] if known else parser.parse_args()
 
-    return args
+    return parser.parse_args()
 
 def main(args: argparse.Namespace):
 
     print(args.input)
         
 if __name__ == "__main__":
-    opt = parse_args()
-    main(opt)
+    args = parse_args()
+    main(args)
